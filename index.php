b913498f6778254264a0ee455206044e7b91c91a
mpfarias <fariasmauricio11@outlook.com>
2024-04-24 10:34:08 -0300

    Update files

--- index.php
+++ index.php
@@ -41,53 +41,86 @@
 </html>
 
 <?php
-    function get_info() {
-        global $database;
 
-        // is post?
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            // validate inputs
-            $username = isset($_POST["username"]) ? trim($_POST["username"]) : null;
-            $email = isset($_POST["email"]) ? trim($_POST["email"]) : null;
-            $password = isset($_POST["password"]) ? trim($_POST["password"]) : null;
+    $server_name = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "forms_db";
 
-            // check if someone is empty
-            $errorMessage = "";
-            
-            if (empty($username)) {
-                $errorMessage .= "Username can't be empty <br>";
-            }
+    // create connection
+    $conn = new mysqli($server_name, $username, $password, $db_name);
 
-            if (empty($email)) {
-                $errorMessage .= "Email can't be empty <br>";
-            }
+    // check connection
+    if ($conn->connect_error) {
+        die("Connection failed: {$conn->connect_error} <br>");
+    }
 
-            if (empty($password)) {
-                $errorMessage .= "password can't be empty <br>";
-            }
+    // $sql = "
+    //     create table form (
+    //         id int auto_increment primary key,
+    //         username varchar(50),
+    //         email varchar(50) NOT NULL,
+    //         password char(56) NOT NULL
+    //     );
+    // ";
 
-            if (!empty($errorMessage)) {
-                return $errorMessage;
-            }
+    
 
-            // Saniteze inputs
-            $username = htmlspecialchars($username);
-            $email = htmlspecialchars($email);
-            $password = htmlspecialchars($password); 
+    // is post?
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        // validate inputs
+        $username = isset($_POST["username"]) ? trim($_POST["username"]) : null;
+        $email = isset($_POST["email"]) ? trim($_POST["email"]) : null;
+        $password = isset($_POST["password"]) ? trim($_POST["password"]) : null;
 
-            // hash password
-            $hashed_password = password_hash($password, PASSWORD_BCRYPT, ["cost" => 12]);
+        echo "Connected successfully";
 
-            $database["Username"] = $username;
-            $database["Email"] = $email;
-            $database["Password"] = $hashed_password;
+        // check if someone is empty
+        $errorMessage = "";
+        
+        if (empty($username)) {
+            $errorMessage .= "Username can't be empty <br>";
         }
-        return "Invalid requested method";
+
+        if (empty($email)) {
+            $errorMessage .= "Email can't be empty <br>";
+        }
+
+        if (empty($password)) {
+            $errorMessage .= "password can't be empty <br>";
+        }
+
+        if (!empty($errorMessage)) {
+            echo $errorMessage;
+        }
+
+        // Saniteze inputs
+        $username = htmlspecialchars($username);
+        $email = htmlspecialchars($email);
+        $password = htmlspecialchars($password); 
+
+        // hash password
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT, ["cost" => 12]);
+
+        $stmt = $conn->prepare("INSERT INTO form (username, email, password) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $username, $email, $hashed_password);
+
+        if ($stmt->execute()) {
+            echo "Query created succefully <br>";
+        } else {
+            echo "Error creating query: {$stmt->error} <br>";
+        }
+    
+        // end connection
+        $stmt->close();
+        $conn->close();
+
+    } else {
+        echo "Invalid requested method";
     }
 
-    get_info();
+    // foreach($database as $key => $value) {
+    //     echo "{$key} : {$value} <br>";
+    // }
 
-    foreach($database as $key => $value) {
-        echo "{$key} : {$value} <br>";
-    }
 ?>
