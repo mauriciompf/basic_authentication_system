363ecae50804c39a2dfa8270dc77096c922c9652
mpfarias <fariasmauricio11@outlook.com>
2024-04-29 17:53:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <div class="wrapper">
         <main class="main">
             <h1 class="wellcome">Wellcome.</h1>
-            <div class="wrapper-form_login">
+            <div class="wrapper-form_login active">
                 <h2 class="heading">Sign-in</h1>
                     <form class="form" action="index.php" method="POST">
                         <div class="input_email input">
@@ -34,12 +34,12 @@
                     </form>
             </div>
 
-            <div class="wrapper-form_register wrapper-form_login active">
+            <div class="wrapper-form_register wrapper-form_login ">
                 <h2 class="heading">Sign-up</h2>
                 <form action="index.php" method="POST" class="form">
                     <div class="input">
                         <label for="f_name">First Name*</label>
-                        <input type="text" name="fname" id="f_name" aria-label="First Name" required>
+                        <input type="text" name="fname" id="f_name" aria-label="First Name">
                     </div>
                     <div class="input">
                         <label for="l_name">Last Name</label>
@@ -47,15 +47,15 @@
                     </div>
                     <div class="input">
                         <label for="r_email">Email*</label>
-                        <input type="email" name="r_email" id="r_email" aria-label="Email" required>
+                        <input type="email" name="r_email" id="r_email" aria-label="Email">
                     </div>
                     <div class="input">
                         <label for="r_password">Password*</label>
-                        <input type="password" name="r_password" id="r_password" aria-label="Password" required>
+                        <input type="password" name="r_password" id="r_password" aria-label="Password">
                     </div>
                     <div class="input">
                         <label for="confirm_pass">Confirm Password*</label>
-                        <input type="password" name="confirm_pass" id="confirm_pass" aria-label="Confirm Password" required>
+                        <input type="password" name="confirm_pass" id="confirm_pass" aria-label="Confirm Password">
                     </div>
                     <div class="form_buttons">
                         <button type="submit" class="btn btn-enter" aria-describedby="Enter with your account">Enter</button>
@@ -64,7 +64,7 @@
                 </form>
             </div>
 
-            <?php include "forms.php"; ?>
+            <?php require "forms.php"; ?>
         </main>
     </div>
     <script type="module" src="app.js"></script>
