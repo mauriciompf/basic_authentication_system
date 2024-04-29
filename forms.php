b01f51417c02bd1be35787c69e2cd841e4b437da
mpfarias <fariasmauricio11@outlook.com>
2024-04-29 17:53:24 -0300

    Update forms.php

--- forms.php
+++ forms.php
@@ -1,13 +1,35 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $first_name = isset($_POST["f_name"]) ? trim($_POST["f_name"]) : null;
-    $last_name = isset($_POST["l_name"]) ? trim($_POST["l_name"]) : null;
-    $r_email = isset($_POST["r_email"]) ? trim($_POST["r_email"]) : null;
-    $r_password = isset($_POST["r_passwod"]) ? trim($_POST["r_password"]) : null;
-    $confirm_pass = isset($_POST["confirm_pass"]) ? trim($_POST["confirm_pass"]) : null;
+try {
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    echo $first_name;
-} else {
-    echo "error";
+        // Disallow whitespace
+        $first_name = isset($_POST["fname"]) ? trim($_POST["fname"]) : null;
+        $last_name = isset($_POST["lname"]) ? trim($_POST["lname"]) : null;
+        $r_email = isset($_POST["r_email"]) ? trim($_POST["r_email"]) : null;
+        $r_password = isset($_POST["r_password"]) ? trim($_POST["r_password"]) : null;
+        $confirm_pass = isset($_POST["confirm_pass"]) ? trim($_POST["confirm_pass"]) : null;
+
+        // Prevent empty inputs
+        if (empty($first_name)) {
+            throw new ErrorException("The first name input is empty");
+        } else if (empty($r_email)) {
+            throw new ErrorException("The email input is empty");
+        } else if (empty($r_password)) {
+            throw new ErrorException("The password input is empty");
+        } else if (empty($confirm_pass)) {
+            throw new ErrorException("The confirm_pass input is empty");
+        }
+
+        // Escape HTML Characters && Sanitize inputs
+        $first_name = htmlspecialchars($first_name, ENT_QUOTES);
+        $last_name = htmlspecialchars($last_name, ENT_QUOTES);
+        $r_email = filter_var($r_email, FILTER_SANITIZE_EMAIL);
+
+        // Hash password
+        $hashed_r_password = password_hash($r_password, PASSWORD_BCRYPT);
+        $hashed_confirm_pass = password_hash($confirm_pass, PASSWORD_BCRYPT);
+    }
+} catch (ErrorException $error) {
+    echo "Error: {$error->getMessage()}";
 }
