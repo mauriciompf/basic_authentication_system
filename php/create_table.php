a8a3fbb90900ff8fef3f9f4e91b219a45fe3fb9e
mpfarias <fariasmauricio11@outlook.com>
2024-05-07 09:48:42 -0300

    Update create_table.php

--- php/create_table.php
+++ php/create_table.php
@@ -8,14 +8,16 @@ function createTable()
 
         $conn = createConnection();
 
+        // $conn->query("USE test");
+
         $sql = "
             CREATE TABLE users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 first_name VARCHAR(50) NOT NULL,
                 last_name VARCHAR(50),
                 email VARCHAR(100) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL,p
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
             )";
 
         $conn->exec($sql);
@@ -24,7 +26,7 @@ function createTable()
 
         $conn = null;
     } catch (PDOException $e) {
-        echo "Error creating table: {$e->getMessage()}";
+        echo "Error creating table: {$e->getMessage()} <br>";
     }
 }
 
