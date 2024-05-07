f32866ddc983a5d9baaf06ac4bf9e410a6e8b99d
mpfarias <fariasmauricio11@outlook.com>
2024-05-07 09:48:48 -0300

    Update database.php

--- php/database.php
+++ php/database.php
@@ -3,16 +3,16 @@
 function createConnection()
 {
     $servername = getenv("localhost");
-    $username = getenv("root@localhost");
-    $password = getenv("1947");
-    $dbname = getenv("r_form");
+    $username = getenv("root");
+    $password = getenv("");
+    $dbname = getenv("test");
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // $conn->query("USE test");
         return $conn;
     } catch (PDOException $e) {
-        echo "Connection failed: {$e->getMessage()}";
+        echo "Connection failed: {$e->getMessage()}<br>";
     }
 }
